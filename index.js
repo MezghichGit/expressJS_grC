dfbae9383de9b6fd24a5b01117b620d2df522477
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-04 11:46:11 +0100

    add + update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express"); // l'import ou le chargement de express
 const app = express();  // la création d'une instance du serveur express
 
+//Middleware
+app.use(express.json());
 
 const equipes = require('./equipes.json');
 app.listen(82, ()=>{
@@ -17,5 +19,18 @@ app.get('/equipes/:id', (req,res)=>{
     const id = parseInt(req.params.id);
     console.log(id);
     const equipe = equipes.find(equipe => equipe.id === id)
+    res.status(200).json(equipe);
+});
+
+app.post('/equipes', (req,res)=>{
+    equipes.push(req.body);
+    res.status(200).json(equipes);
+});
+
+app.put('/equipes/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    let equipe = equipes.find(equipe => equipe.id === id)
+    equipe.name = req.body.name;
+    equipe.country = req.body.country;
     res.status(200).json(equipe);
 });
