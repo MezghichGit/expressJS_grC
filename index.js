cfd47123c7273086252f62ca8cbfdd908ff9e420
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-04 11:37:06 +0100

    méthode get avec paam

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.listen(82, ()=>{
     console.log("Bienvenue à node expressJS");
 });
 
+
 app.get('/equipes', (req,res)=>{
-    res.send("<h1 align=center> Hello mes équipes de foot </h1>");
+    //res.send("<h1 align=center> Hello mes équipes de foot </h1>");
+    res.status(200).json(equipes);
+});
+
+app.get('/equipes/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const equipe = equipes.find(equipe => equipe.id === id)
+    res.status(200).json(equipe);
 });
