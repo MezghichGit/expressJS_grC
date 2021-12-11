d4d715687002bd2651b166e62aa57d263c24d5bb
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-11 16:52:23 +0100

    correction exo Joueur

--- index.js
+++ index.js
@@ -3,41 +3,215 @@ const app = express();  // la création d'une instance du serveur express
 
 //Middleware
 app.use(express.json());
-
-const equipes = require('./equipes.json');
+//const equipes = require('./equipes.json');
 app.listen(82, ()=>{
     console.log("Bienvenue à node expressJS");
 });
 
-
-app.get('/equipes', (req,res)=>{
-    //res.send("<h1 align=center> Hello mes équipes de foot </h1>");
-    res.status(200).json(equipes);
+/**
+ * Importation du client MongoClient & connexion à la DB
+ */
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'sesame4c';
+let db 
+MongoClient.connect(url, function(err, client) {
+ console.log("Connexion réussi avec Mongo");
+ db = client.db(dbName);
 });
 
-app.get('/equipes/:id', (req,res)=>{
-    const id = parseInt(req.params.id);
-    console.log(id);
-    const equipe = equipes.find(equipe => equipe.id === id)
-    res.status(200).json(equipe);
-});
+app.get('/equipes', (req,res) => {
+      db.collection('equipe').find({}).toArray(function(err, docs) {
+          if (err) {
+              console.log(err)
+              throw err
+          }
+          res.status(200).json(docs)
+        }) 
+    })
+    
+    app.get('/equipes/:id', async (req,res) => {
+          const id = parseInt(req.params.id)
+          try {
+              const docs = await db.collection('equipe').find({id}).toArray()
+              res.status(200).json(docs)
+          } catch (err) {
+              console.log(err)
+              throw err
+          }
+        })
+        
 
-app.post('/equipes', (req,res)=>{
-    equipes.push(req.body);
-    res.status(200).json(equipes);
-});
+        app.post('/equipes', async (req,res) => {
+              try {
+                  const equipeData = req.body
+                  const equipe = await db.collection('equipe').insertOne(equipeData)
+                  res.status(200).json(equipe)
+              } catch (err) {
+                  console.log(err)
+                  throw err
+              }
+            })
+            
+    app.put('/equipes/:id', async (req,res) => {
+                  try {
+                      const id = parseInt(req.params.id)
+                      const replacementEquipe = req.body
+                      const equipe = await db.collection('equipe').replaceOne({id},replacementEquipe)
+                      res.status(200).json(equipe)
+                  } catch (err) {
+                      console.log(err)
+                      throw err
+                  }
+                })
+                
+                app.delete('/equipes/:id', async (req,res) => {
+                      try {
+                          const id = parseInt(req.params.id)
+                          const equipe = await db.collection('equipe').deleteOne({id})
+                          res.status(200).json(equipe)
+                      } catch (err) {
+                          console.log(err)
+                          throw err
+                      } 
+                    })
+                    
 
-app.put('/equipes/:id', (req,res)=>{
-    const id = parseInt(req.params.id);
-    let equipe = equipes.find(equipe => equipe.id === id)
-    equipe.name = req.body.name;
-    equipe.country = req.body.country;
-    res.status(200).json(equipe);
-});
+                    
+               /* TD joueur */
 
-app.delete('/equipes/:id', (req,res)=>{
-    const id = parseInt(req.params.id);
-    let equipe = equipes.find(equipe => equipe.id === id)
-    equipes.splice(equipes.indexOf(equipe),1)
-    res.status(200).json(equipes);
-});
+// question1
+
+app.get('/joueurs', (req,res) => {
+    db.collection('joueur').find({}).toArray(function(err, docs
+    ){
+        if (err){
+            console.log(err)
+            throw err
+        }
+        res.status(200).json(docs)
+    })
+ })
+ 
+ app.get('/joueurs/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    db.collection('joueur').find({id}).toArray(function(err, docs
+    ){
+        if (err){
+            console.log(err)
+            throw err
+        }
+        res.status(200).json(docs)
+    })
+ })
+ 
+ app.post('/joueurs', async (req,res) => {
+  try {
+    const joueurData = req.body
+    const joueur = await db.collection ('joueur').insertOne(joueurData)
+    res.status(200).json(joueur)
+  } catch (err)
+    {
+            console.log(err)
+            throw err
+        }
+ })
+ 
+ app.put('/joueurs/:id', async (req,res) => {
+    try {
+      const id = parseInt(req.params.id)
+      const newjoueur = req.body
+      const oldjoueur = (await db.collection ('joueur').find({id}).toArray())[0]
+      oldjoueur.nom=newjoueur.nom;
+      oldjoueur.numero=newjoueur.numero;
+       await db.collection ('joueur').replaceOne({id},oldjoueur)
+ 
+      res.status(200).json(oldjoueur)
+    } catch (err)
+      {
+              console.log(err)
+              throw err
+          }
+ })
+ 
+    
+ app.delete('/joueurs/:id', async (req,res) => {
+  try {
+    const id = (req.params.id)
+    const joueur = await db.collection ('joueur').deleteOne({id})
+    res.status(200).json(joueur)
+  } catch (err)
+    {
+            console.log(err)
+            throw err
+        }
+ })
+
+ // question2
+app.get('/equipes/:id/joueurs', (req,res) => {
+    const idEquipe = parseInt(req.params.id)
+    db.collection('joueur').find({idEquipe}).toArray(function(err, docs
+    ){
+        if (err){
+            console.log(err)
+            throw err
+        }
+        res.status(200).json(docs)
+    })
+ })
+
+ // question3
+
+app.get('/joueurs/:id/equipes',async (req,res) => {
+    const id = parseInt(req.params.id)
+    const joueur =await  db.collection('joueur').findOne({id});
+ 
+    const equipe =await db.collection('equipe').findOne({id:joueur.idEquipe});
+    res.status(200).json(equipe)
+    
+ })
+
+ //question4
+app.get('/joueurbyName/:nom', (req,res) => {
+    const nom = req.params.nom
+    db.collection('joueur').find({nom}).toArray(function(err, docs
+    ){
+        if (err){
+            console.log(err)
+            throw err
+        }
+        res.status(200).json(docs)
+    })
+ })
+// app.get('/equipes', (req,res)=>{
+//     //res.send("<h1 align=center> Hello mes équipes de foot </h1>");
+//     res.status(200).json(equipes);
+// });
+
+// app.get('/equipes/:id', (req,res)=>{
+//     const id = parseInt(req.params.id);
+//     console.log(id);
+//     const equipe = equipes.find(equipe => equipe.id === id)
+//     res.status(200).json(equipe);
+// });
+
+// app.post('/equipes', (req,res)=>{
+//     equipes.push(req.body);
+//     res.status(200).json(equipes);
+// });
+
+// app.put('/equipes/:id', (req,res)=>{
+//     const id = parseInt(req.params.id);
+//     let equipe = equipes.find(equipe => equipe.id === id)
+//     equipe.name = req.body.name;
+//     equipe.country = req.body.country;
+//     res.status(200).json(equipe);
+// });
+
+// app.delete('/equipes/:id', (req,res)=>{
+//     const id = parseInt(req.params.id);
+//     let equipe = equipes.find(equipe => equipe.id === id)
+//     equipes.splice(equipes.indexOf(equipe),1)
+//     res.status(200).json(equipes);
+// });
+// partie mongodb
